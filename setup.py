fd4f257f1143d591fd746b982879c0020b8e20ef
Theron Luhn <theron@luhn.com>
2021-01-08 22:43:23 -0800

    Black setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-
-VERSION = '0.1.0'
+VERSION = "0.1.0"
 CLASSIFIERS = [
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Framework :: Pyramid',
-    'Development Status :: 4 - Beta',
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Framework :: Pyramid",
+    "Development Status :: 4 - Beta",
 ]
 
 
 REQUIRES = [
-    'pyramid>=1.9,<2',
-    'celery>=4,<5',
-    'venusian',
+    "pyramid>=1.9,<2",
+    "celery>=4,<5",
+    "venusian",
 ]
 EXTRAS_REQUIRE = {
-    'testing': ['pytest>=6,<7'],
+    "testing": ["pytest>=6,<7"],
 }
 
 DESCRIPTION = (
-    'Bring parity to Pyramid and Celery by creating a full Pyramid '
-    'application in the Celery worker and providing a request object for each '
-    'task.'
+    "Bring parity to Pyramid and Celery by creating a full Pyramid "
+    "application in the Celery worker and providing a request object for each "
+    "task."
 )
 
 
@@ -32,17 +31,17 @@ with open("README.md", "r") as fh:
 
 
 setup(
-    name='pyramid-tasks',
+    name="pyramid-tasks",
     version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/luhn/pyramid-tasks/',
-    author='Theron Luhn',
-    author_email='theron@luhn.com',
+    url="https://github.com/luhn/pyramid-tasks/",
+    author="Theron Luhn",
+    author_email="theron@luhn.com",
     classifiers=CLASSIFIERS,
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=REQUIRES,
     extras_require=EXTRAS_REQUIRE,
 )
