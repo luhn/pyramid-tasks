48ddd78c29e8d6ec7d7625f2cfb2b3d32b512c14
Theron Luhn <theron@luhn.com>
2025-09-13 09:00:59 -0700

    Lint fix.

--- pyramid_tasks/transaction.py
+++ pyramid_tasks/transaction.py
@@ -3,13 +3,14 @@ For backwards compatibility.  For new proejcts, use
 ``pyramid_tasks.contrib.pyramid_tm`` instead.
 
 """
+
 from .contrib.pyramid_tm import transaction_task_deriver
 
 
 def includeme(config):
-    config.include('.contrib.pyramid_tm')
+    config.include(".contrib.pyramid_tm")
 
 
 __all__ = [
-    'transaction_task_deriver',
+    "transaction_task_deriver",
 ]
